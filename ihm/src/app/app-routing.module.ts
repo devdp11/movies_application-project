92543c0ca9ab43c90d76dec25d4991226dfc9d01
Diogo Pinheiro <120480585+diogoPinheiro11@users.noreply.github.com>
2023-05-15 16:52:12 +0100

    some add ons

--- ihm/src/app/app-routing.module.ts
+++ ihm/src/app/app-routing.module.ts
@@ -13,7 +13,8 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },  {
+  },
+  {
     path: 'action',
     loadChildren: () => import('./action/action.module').then( m => m.ActionPageModule)
   }
