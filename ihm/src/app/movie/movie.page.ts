9cddb5fc20c5ecb008cb24944fa4cc7b10e15140
Diogo Pinheiro <120480585+diogoPinheiro11@users.noreply.github.com>
2023-05-25 18:27:24 +0100

    added movies / changed variables names

--- ihm/src/app/movie/movie.page.ts
+++ ihm/src/app/movie/movie.page.ts
@@ -20,7 +20,7 @@ interface Movies {
 })
 export class MoviePage implements OnInit {
   
-  public receivedMovie: any;
+  public CaseMovie: any;
   public movie: Movies | undefined;
   public dataMovies: Movies[] = [];
 
@@ -28,13 +28,18 @@ export class MoviePage implements OnInit {
 
 
   ngOnInit() {
-    this.receivedMovie = this.route.snapshot.paramMap.get('id');
+    this.CaseMovie = this.route.snapshot.paramMap.get('id');
       fetch('./assets/data/movies.json')
       .then(res => res.json())
       .then(json => {
         this.dataMovies = json;
-        this.movie = this.dataMovies.find((movie) => movie.id === this.receivedMovie);
+        this.movie = this.dataMovies.find((movie) => movie.id === this.CaseMovie);
       });
+  }
+
+  goWatchLater(){
+    console.log('The watch later button was clicked');
+    
   }
 
 }
