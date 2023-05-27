d216b25f9b3f5ace0522639f0642cf749b9605fb
Diogo Pinheiro <120480585+diogoPinheiro11@users.noreply.github.com>
2023-05-27 23:25:57 +0100

    commit before capacitor implementation

--- ihm/src/app/movie/movie.page.ts
+++ ihm/src/app/movie/movie.page.ts
@@ -29,7 +29,7 @@ export class MoviePage implements OnInit {
 
   constructor(private route: ActivatedRoute, private router: Router, private playlistService: PlaylistsService, private alerta: AlertController) {}
 
-
+  // esta função está a recuperar o valor do parâmetro 'id' onde carrega um ficheiro JSON com as informações relativas a filmes. Asseguir ao encontrar o filme que corresponde ao id em causa, o resultado é armazenado para no 'movie' para ser usado mais tarde, para guardar na playlist ver mais tarde e para carregar os dados da página.
   ngOnInit() {
     this.CaseMovie = this.route.snapshot.paramMap.get('id');
       fetch('./assets/data/movies.json')
@@ -39,7 +39,7 @@ export class MoviePage implements OnInit {
         this.movie = this.dataMovies.find((movie) => movie.id === this.CaseMovie);
       });
   }
-
+  // esta função verifica inicialmente se o filme em causa ja se encontra na playlist de 'ver mais tarde' através do seu id, aparecendo um alert na ecrã a informar do mesmo, e caso o mesmo não esteja, o filme é adicionado juntamente com as suas informações a serem transmitidas na página da playlist, aparecendo também um alerta no ecrã a informar que o filme foi colocado na playlist
   async goWatchLater() {
     if (this.movie) {
       const movieExists = await this.playlistService.checkIfMovieExists(this.movie.id);
@@ -58,7 +58,7 @@ export class MoviePage implements OnInit {
       console.log('No movie found');
     }
   }
-  
+  // esta função é chamada na função 'goWatchLater' quando a mesma confirma que o filme já se encontra na playlist
   async showAlert(message: string) {
     const alert = await this.alerta.create({
       message: message,
@@ -66,7 +66,7 @@ export class MoviePage implements OnInit {
     });
     await alert.present();
   }
-
+  // esta função é chamada na função 'goWatchLater' quando a mesma coloca com sucesso o filme na playlist
   async showSuccessAlert(message: string) {
     const alert = await this.alerta.create({
       message: message,
@@ -74,7 +74,7 @@ export class MoviePage implements OnInit {
     });
     await alert.present();
   }
-  
+  // esta função é chamada na função 'goWatchLater' quando a mesma não consegue colocar o filme na playlist devido a um erro
   async showErrorAlert(message: string) {
     const alert = await this.alerta.create({
       message: message,
@@ -82,7 +82,7 @@ export class MoviePage implements OnInit {
     });
     await alert.present();
   }
-
+  // esta função é chamada quando o utilizador clica no botão 'Avalie o filme' e informa o mesmo que avaliou o filme
   async rating(){
     const alerta = await this.alerta.create({
       message: 'Avaliaste com sucesso o filme',
